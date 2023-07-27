757837085d453a23336ed3e2c124ac869d579bfb
PetrusAriaa <petrusariacr25@gmail.com>
2023-07-28 00:26:26 +0700

    feat: Not Fixed - create display for Katalog

--- src/app/katalog/page.js
+++ src/app/katalog/page.js
@@ -1,7 +1,91 @@
+"use client"
+import Navbar from "../components/navbar"
+import { BsFillCaretDownFill, BsFillCaretUpFill } from "react-icons/bs"
+import category from "./category"
+import Link from "next/link"
+import { useState } from "react"
+import SMH from '../../../public/dummy/gkpi1.jpeg'
+import Image from "next/image"
+import products from "./product"
+
 export default function Katalog(){
+  const [dropIsClicked, setDropIsClicked] = useState(false)
+
+  const Category = () => {
+    return (
+      <div className="absolute min-w-[15em]">
+        <button
+        onClick={() => setDropIsClicked(!dropIsClicked)}
+        className={"relative w-full px-4 py-2 flex flex-row items-center text-neutral-400 justify-between border border-neutral-400 " + (
+          dropIsClicked ? "border-b-0" : ""
+        )}>
+          Category
+          {
+            dropIsClicked ? <BsFillCaretUpFill /> : <BsFillCaretDownFill />
+          }
+          
+        </button>
+        <div className={"relative bg-white flex flex-col items-start border text-neutral-400 border-neutral-400 " +
+        (
+          dropIsClicked ? "block" : "hidden"
+        )}>
+          {
+            category.map((item, i) => {
+              return (
+                <Link
+                onClick={() => console.log(item.link)}
+                href={{pathname:"/katalog", query: (item.link !== "" ? { category: item.link} : "")}}
+                className="w-full"
+                key={i}>
+                  <div className="hover:bg-neutral-300/50 px-4 py-2">
+                  {item.title}
+                  </div>
+                </Link>
+              )
+            })
+          }
+        </div>
+      </div>
+    )
+  }
+
+  const Card = ({img, title, price, src}) => {
+    return(
+      <div className="border items-center">
+        <div className="h-96 object-fill overflow-hidden">
+          <Image src={img} width={1000} height={1} className="w-full"/>
+        </div>
+        <h1>{title}</h1>
+        <h1>{price}</h1>
+        <button onClick={()=>alert(src)}>Order</button>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <h1>Katalog Online</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="min-h-screen flex flex-col items-center pt-36 pb-48">
+        <div className="w-4/5 space-y-16">
+          <div className="flex justify-end">
+            <Category />
+          </div>
+          <div className="bg-red-200 min-h-min grid grid-cols-2 md:grid-cols-3 gap-6">
+            {
+              products.map((item, i) => {
+                return(
+                  <Card
+                  img={item.img}
+                  title={item.title}
+                  price={item.price}
+                  src={item.src}
+                  key={i}/>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
---
+++ src/app/katalog/product.js
@@ -0,0 +1,46 @@
+const products = [
+  {
+    img: '/dummy/gkpi1.jpeg',
+    title: 'Bacang',
+    price: 'Rp25.000',
+    src: 'wa.me/'
+  },
+  {
+    img: '/dummy/gkpi2.jpeg',
+    title: 'Lele Bumbu',
+    price: 'Rp35.000',
+    src: 'wa.me/'
+  },
+  {
+    img: '/dummy/gkpi3.jpeg',
+    title: 'Launch Bag',
+    price: 'Rp50.000',
+    src: 'wa.me/'
+  },
+  {
+    img: '/dummy/gkpi4.jpeg',
+    title: 'Hand Bag',
+    price: 'Rp50.000',
+    src: 'wa.me/'
+  },
+  {
+    img: '/dummy/gkpi5.jpeg',
+    title: 'Launchbox Bag',
+    price: 'Rp50.000',
+    src: 'wa.me/'
+  },
+  {
+    img: '/dummy/gkpi6.jpeg',
+    title: 'Sling Bag',
+    price: 'Rp50.000',
+    src: 'wa.me/'
+  },
+  {
+    img: '/dummy/gkpi7.jpeg',
+    title: 'Macrame Clutchbag',
+    price: 'Rp48.000',
+    src: 'wa.me/'
+  },
+]
+
+module.exports = products
