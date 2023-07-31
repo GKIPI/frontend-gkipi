95923e32b1a4a0eade432bfd69d4a3c2c51dc6df
PetrusAriaa <petrusariacr25@gmail.com>
2023-07-31 19:57:40 +0700

    feat: Create page Home

---
+++ src/app/admin/dashboard/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex flex-row justify-center h-96">
+      <div className="w-3/4 flex items-center">
+        <h1 className="font-montserrat text-4xl font-semibold text-center">
+          Welcome to Admin Dashboard GKI Pondok Indah
+        </h1>
+      </div>
+    </div>
+  );
+}
