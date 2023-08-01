71c8d67fac0e994d5fd27f73c6f6524bba138f46
PetrusAriaa <petrusariacr25@gmail.com>
2023-08-02 04:19:05 +0700

    fix: map through list

--- src/app/admin/dashboard/JobSeeker.jsx
+++ src/app/admin/dashboard/JobSeeker.jsx
@@ -1,4 +1,6 @@
 "use client";
+
+import {useState} from "react";
 import {
   AiOutlineEye,
   AiOutlineFileSearch,
@@ -6,19 +8,19 @@ import {
   AiOutlineDelete,
   AiOutlineFileAdd,
 } from "react-icons/ai";
+
 import JobSeekerCVModals from "./AdminDashboardModals/JobSeekerCVModals";
-import {useState} from "react";
+import JobSeekerDetailsModal from "./AdminDashboardModals/JobSeekerDetailsModal";
+import ConfirmDeleteModal from "./AdminDashboardModals/ConfirmDeleteModal";
+import {countJobseeker} from "../../../../helper/requestCounter";
+
+import {jobseeker} from "./test/jobseeker";
 
 export default function JobSeeker() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
+  const [isModalCVOpen, setIsModalCVOpen] = useState(false);
+  const [isModalDetailsOpen, setIsModalDetailsOpen] = useState(false);
+  const [confirmDeleteModal, setConfirmDeleteModal] = useState(false);
+  const [currItem, setCurrItem] = useState(0);
 
   return (
     <section className="space-y-10">
@@ -28,7 +30,7 @@ export default function JobSeeker() {
       <div className="space-y-4">
         <div className="flex justify-end">
           <button className="font-montserrat bg-zinc-800 text-slate-200 px-4 py-2 hover:bg-transparent hover:text-zinc-800 hover:outline hover:outline-2 hover:outline-zinc-800 transi duration-200">
-            Review (5)
+            Review ({countJobseeker(jobseeker)})
           </button>
         </div>
         <div>
@@ -43,65 +45,95 @@ export default function JobSeeker() {
               </tr>
             </thead>
             <tbody className="text-sm font-montserrat text-zinc-600">
-              <tr>
-                <td className="py-4 pl-4 border-b border-zinc-800">
-                  <p className="line-clamp-1">Joanna Anderson</p>
-                </td>
-                <td className="border-b border-zinc-800">
-                  <p className="line-clamp-1">IT Undergraduate</p>
-                </td>
-                <td className="border-b border-zinc-800">
-                  <p className="line-clamp-1">
-                    Adobe Illustrator, Adobe Photosop
-                  </p>
-                </td>
-                <td className="border-b border-zinc-800">
-                  <div className="flex flex-row items-center gap-3">
-                    <button>
-                      <AiOutlineFileAdd
-                        title="add/update"
-                        size={25}
-                        className="hover:text-amber-400"
-                      />
-                    </button>
-                    <button onClick={handleOpenModal}>
-                      <AiOutlineFileSearch
-                        title="view PDF"
-                        size={25}
-                        className="hover:text-blue-400"
-                      />
-                    </button>
-                  </div>
-                </td>
-                <td className="border-b border-zinc-800">
-                  <div className="flex flex-row items-center gap-3">
-                    <button>
-                      <AiOutlineEye
-                        size={25}
-                        title="view"
-                        className="hover:text-blue-400"
-                      />
-                    </button>
-                    <button>
-                      <AiOutlineForm
-                        size={25}
-                        title="edit"
-                        className="hover:text-amber-400"
-                      />
-                    </button>
-                    <button>
-                      <AiOutlineDelete
-                        size={25}
-                        title="delete"
-                        className="hover:text-red-400"
-                      />
-                    </button>
-                  </div>
-                </td>
-              </tr>
+              {jobseeker.map((item, i) => {
+                if (!item.isApproved) return null;
+                return (
+                  <tr>
+                    <td className="py-4 pl-4 border-b border-zinc-800">
+                      <p className="line-clamp-1">{item.nama}</p>
+                    </td>
+                    <td className="border-b border-zinc-800">
+                      <p className="line-clamp-1">{item.pendidikan}</p>
+                    </td>
+                    <td className="border-b border-zinc-800">
+                      <p className="line-clamp-1">{item.keahlian}</p>
+                    </td>
+                    <td className="border-b border-zinc-800">
+                      <div className="flex flex-row items-center gap-3">
+                        <button>
+                          <AiOutlineFileAdd
+                            title="add/update"
+                            size={25}
+                            className="hover:text-amber-400"
+                          />
+                        </button>
+                        <button
+                          onClick={() => {
+                            setIsModalCVOpen(true);
+                          }}
+                        >
+                          <AiOutlineFileSearch
+                            title="view PDF"
+                            size={25}
+                            className="hover:text-blue-400"
+                          />
+                        </button>
+                      </div>
+                    </td>
+                    <td className="border-b border-zinc-800">
+                      <div className="flex flex-row items-center gap-3">
+                        <button
+                          onClick={() => {
+                            setCurrItem(i);
+                            setIsModalDetailsOpen(true);
+                          }}
+                        >
+                          <AiOutlineEye
+                            size={25}
+                            title="view"
+                            className="hover:text-blue-400"
+                          />
+                        </button>
+                        <button>
+                          <AiOutlineForm
+                            size={25}
+                            title="edit"
+                            className="hover:text-amber-400"
+                          />
+                        </button>
+                        <button>
+                          <AiOutlineDelete
+                            onClick={() => {
+                              setCurrItem(i);
+                              setConfirmDeleteModal(true);
+                            }}
+                            size={25}
+                            title="delete"
+                            className="hover:text-red-400"
+                          />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
-          <JobSeekerCVModals isOpen={isModalOpen} onClose={handleCloseModal} />
+          <JobSeekerCVModals
+            isOpen={isModalCVOpen}
+            src={jobseeker[currItem]}
+            onClose={() => setIsModalCVOpen(false)}
+          />
+          <JobSeekerDetailsModal
+            src={jobseeker[currItem]}
+            isOpen={isModalDetailsOpen}
+            onClose={() => setIsModalDetailsOpen(false)}
+          />
+          <ConfirmDeleteModal
+            src={currItem}
+            isOpen={confirmDeleteModal}
+            onClose={() => setConfirmDeleteModal(false)}
+          />
         </div>
       </div>
     </section>
