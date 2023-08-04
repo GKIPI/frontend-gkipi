eb604fe3f3f27c2d1cf37bcc8e51d52188e57382
inamnurul <aam.inam7310@gmail.com>
2023-08-04 13:35:25 +0700

    feat: katalog dashboard for user

--- src/app/user/katalog/page.js
+++ src/app/user/katalog/page.js
@@ -5,61 +5,51 @@ import { useState } from "react";
 
 export default function UserDashboard() {
     const [email, setEmail] = useState("");
-    const [jobTitle, setJobTitle] = useState("");
-    const [company, setCompany] = useState("");
-    const [jobLocation, setJobLocation] = useState("");
+    const [title, setTitle] = useState("");
+    const [prize, setPrize] = useState("");
+    const [tag, setTag] = useState([]);
     const [imageFile, setImageFile] = useState(null);
     const [data, setData] = useState({ email: "loading...", jobTitle: "loading...", company: "loading...", tag: "loading...", jobLocation: "loading..." })
-    const dummyJobVacancies = [
+
+    
+    const dummyKatalog = [
         {
-          jobTitle: "Software Engineer",
-          company: "TechCo",
-          jobLocation: "New York, USA",
-          tag: "Software Development",
-          base64Image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUA",
+          title: "Sample Product 1",
+          prize: "1000",
+          tag: "Beauty & Health",
+          image: "",
         },
         {
-          jobTitle: "Marketing Manager",
-          company: "Globex",
-          jobLocation: "London, UK",
-          tag: "Marketing",
-          base64Image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA",
+          title: "Sample Product 2",
+          prize: "500",
+          tag: "Electronics",
+          image: "",
         },
         {
-          jobTitle: "Accountant",
-          company: "Numbers Inc.",
-          jobLocation: "Sydney, Australia",
-          tag: "Finance",
-          base64Image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABs",
+          title: "Sample Product 3",
+          prize: "200",
+          tag: "Food & Beverage",
+          image: "",
         },
       ];
-      
-    const [jobVacancies, setJobVacancies] = useState(dummyJobVacancies);
+    const [katalog, setKatalog] = useState(dummyKatalog);
 
     const handleFileChange = (e) => {
         const file = e.target.files[0];
         setImageFile(file);
     };
 
-    const tagIndustry = [
-        "Industrial / Manufacturing",
-        "Insurance",
-        "FMCG",
-        "Media & Agency",
-        "Financial Service",
+    const tagOption= [
+        "Beauty & Health",
+        "Sport",
         "Property",
-        "Retail"
+        "Fashion & Accessories",
+        "Food & Beverage",
+        "Electronics",
+        "Book & Stationary",
+        "Other"
     ];
 
-    const tagTitle = [
-        "Staff",
-        "Supervisor",
-        "Manager",
-        "General Manager",
-        "Director",
-    ];
-
-    
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -67,24 +57,22 @@ export default function UserDashboard() {
         // Create a form data object to send the data to the server
         const formData = new FormData();
         formData.append("tag", tag);
-        formData.append("jobTitle", jobTitle);
-        formData.append("company", company);
-        formData.append("location", jobLocation);
+        formData.append("title", title);
+        formData.append("prize", prize);
         formData.append("image", imageFile);
 
         try {
             // Access the BLOB data of the image
             const imageBlob = new Blob([imageFile], { type: imageFile.type });
 
-            // Add the current job vacancy data to the list of job vacancies
-            setJobVacancies((prevJobVacancies) => [
-                ...prevJobVacancies,
+            // Add the current katalog data to the list of job vacancies
+            setJobVacancies((prevKatalog) => [
+                ...prevKatalog,
                 {
-                    jobTitle,
-                    company,
-                    jobLocation,
+                    title,
+                    prize,
                     tag,
-                    base64Image,
+                    image: base64Image,
                 },
             ]);
             // Convert the image data to a Base64-encoded string
@@ -96,6 +84,7 @@ export default function UserDashboard() {
 
                 // Add the Base64 image to the form data
                 formData.append("base64Image", base64Image);
+
                 // Show success message or perform other actions on successful submission
                 console.log("Seeker data submitted successfully!");
             };
@@ -107,10 +96,10 @@ export default function UserDashboard() {
     };
 
     // Step 3: Create a function to handle deleting a specific job vacancy
-    const handleDeleteJobVacancy = (index) => {
-        const updatedJobVacancies = [...jobVacancies];
-        updatedJobVacancies.splice(index, 1);
-        setJobVacancies(updatedJobVacancies);
+    const handleDeleteKatalog = (index) => {
+        const updatedKatalog = [...katalog];
+        updatedKatalog.splice(index, 1);
+        setKatalog(updatedKatalog);
     };
 
     return (
@@ -125,52 +114,32 @@ export default function UserDashboard() {
                     <form onSubmit={handleSubmit} className="bg-tertiary lg:w-full h-screen p-5 flex flex-col justify-center">
                         <h1 className="font-bold text-[3rem] px-4 self-center">Lets make the world better!</h1>
                         <label className="border-2 p-3 w-full border-black flex flex-row justify-between text-lg items-center rounded-lg my-2">
-                            Job Title:
-                            <input className="border-2 border-black w-[50%] p-1 rounded-lg" type="text" value={jobTitle} onChange={(e) => setJobTitle(e.target.value)} />
+                            Title:
+                            <input className="border-2 border-black w-[50%] p-1 rounded-lg" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
                         </label>
                         <label className="border-2 p-3 w-full border-black flex flex-row justify-between text-lg items-center rounded-lg my-2">
-                            Company:
-                            <input className="border-2 border-black w-[50%] p-1 rounded-lg" type="text" value={company} onChange={(e) => setCompany(e.target.value)} />
-                        </label>
-                        <label className="border-2 p-3 w-full border-black flex flex-row justify-between text-lg items-center rounded-lg my-2">
-                            Job location:
-                            <input className="border-2 border-black w-[50%] p-1 rounded-lg" type="text" value={jobLocation} onChange={(e) => setJobLocation(e.target.value)} />
+                            Prize:
+                            <input className="border-2 border-black w-[50%] p-1 rounded-lg" type="number" value={prize} onChange={(e) => setPrize(e.target.value)} />
                         </label>
                         <label className="border-2 p-3 w-full border-black flex flex-row justify-between text-lg items-center rounded-lg my-2">
                             Upload Image:
                             <input className="w-[50%] p-1" type="file" accept="image/*" onChange={handleFileChange} />
                         </label>
                         <label className="border-2 p-3 w-full border-black flex flex-row justify-between text-lg items-center rounded-lg my-2">
-                            Industry tag:
+                            Tag:
                             <select
                                 className="border-2 border-black w-[50%] p-1 rounded-lg"
-                                value={email}
+                                value={tag}
                                 onChange={(e) => setEmail(e.target.value)}
                             >
                                 <option value="" disabled>Select a Tag</option>
-                                {tagIndustry.map((tagOption) => (
+                                {tagOption.map((tagOption) => (
                                     <option key={tagOption} value={tagOption}>
                                         {tagOption}
                                     </option>
                                 ))}
                             </select>
                         </label>
-                        <label className="border-2 p-3 w-full border-black flex flex-row justify-between text-lg items-center rounded-lg my-2">
-                            Title tag:
-                            <select
-                                className="border-2 border-black w-[50%] p-1 rounded-lg"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            >
-                                <option value="" disabled>Select a Tag</option>
-                                {tagTitle.map((tagOption) => (
-                                    <option key={tagOption} value={tagOption}>
-                                        {tagOption}
-                                    </option>
-                                ))}
-                            </select>
-                        </label>
-
                         <button className="bg-black text-white text-center py-4 rounded-md my-2 self-end w-[25%] hover:text-primary border-2 border-primary hover:bg-white" type="submit" >Submit</button>
                     </form>
                 </div>
@@ -178,23 +147,23 @@ export default function UserDashboard() {
                     <div className="max-w-full h-full p-4" style={{ borderRadius: "10px", background: "#FAFAFA", boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset" }}>
                         <div className="h-[80vh]">
                             <h1 className="font-bold text-[3rem] px-4 self-center">Preview</h1>
-                            {jobVacancies.length > 0 ? (
-                                jobVacancies.map((jobVacancy, index) => (
+                            {katalog.length > 0 ? (
+                                katalog.map((katalog, index) => (
                                     <div key={index} className="border-2 p-3 w-full border-black flex flex-row justify-between text-lg items-center rounded-lg my-2">
                                         <div>
-                                        Job Title :
-                                        <div>{jobVacancy.jobTitle}</div>
+                                        Title :
+                                        <div>{katalog.title}</div>
                                         </div>
                                         <div>
                                         <button
                                             className="bg-primary text-white px-4 py-2 rounded-md hover:text-primary border-2 border-primary hover:bg-white"
-                                            onClick={() => handleViewJobVacancy(index)}
+                                            onClick={() => handleViewKatalog(index)}
                                         >
                                             View
                                         </button>{" "}
                                         <button
                                             className="bg-red-600 text-white px-4 py-2 rounded-md hover:text-red-600 border-2 border-red-600 hover:bg-white"
-                                            onClick={() => handleDeleteJobVacancy(index)}
+                                            onClick={() => handleDeleteKatalog(index)}
                                         >
                                             Delete
                                         </button>
