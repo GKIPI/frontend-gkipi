da4ffca2c872e876aeb40544e691e58020476e67
PetrusAriaa <petrusariacr25@gmail.com>
2023-08-13 18:11:38 +0700

    fix: resolve metadata export

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,16 @@
-"use client"
+// "use client"
 import './globals.css'
-import { Inter } from 'next/font/google'
-import Footer from './components/footer'
+// import { Inter } from 'next/font/google'
+// import Footer from './components/footer'
 import 'react-toastify/dist/ReactToastify.css';
 import AuthProvider from './components/authProvider'
 import { ToastContainer } from 'react-toastify'
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
-// export const metadata = {
-//   title: 'Komunitas Profesi Grha Persahabatan',
-//   description: 'Komunitas Profesi Grha Persahabatan GKIPI',
-// }
+export const metadata = {
+  title: 'Komunitas Profesi Grha Persahabatan',
+  description: 'Komunitas Profesi Grha Persahabatan GKIPI',
+}
 
 export default function RootLayout({ children }) {
   return (
