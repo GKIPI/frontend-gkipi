d54600457b6dab50ad99e2887ffa38652b74d1db
inamnurul <aam.inam7310@gmail.com>
2023-08-17 20:57:56 +0700

    fix: responsive disclaimer component

--- src/app/(private_route)/components/DisclaimerModal.js
+++ src/app/(private_route)/components/DisclaimerModal.js
@@ -6,17 +6,17 @@ export default function DisclaimerModal ({ isOpen, setIsOpenClose, handlesubmit 
     return (
         <>
         {isOpen? <div className="fixed inset-0 flex items-center justify-center z-50 bg-slate-800/25 backdrop-blur-sm">
-            <div className="bg-slate-50 w-[50%] min-h-[40rem] shadow-lg flex flex-col gap-8 rounded-lg px-5 py-5 font-montserrat">
-                <div className="flex flex-row justify-end items-center bg-red-500/25">
+            <div className="bg-slate-50 w-[80vw] md:w-[50%] max-h-[80vh] shadow-lg flex flex-col gap-8 rounded-lg px-5 py-5 font-montserrat">
+                <div className="flex flex-row justify-end items-center">
                     <button onClick={() => {
                         setIsOpenClose(false)
                     }}>
                         <FiX size={20} />
                     </button>
                 </div>
-                <div className="bg-red-500/25 flex flex-col">
+                <div className=" flex flex-col">
                     <h1 className="text-center text-3xl font-bold font-montserrat">Disclaimer</h1>
-                    <ol className="list-decimal pl-6 text-justify flex flex-col gap-3">
+                    <ol className="list-decimal pl-6 text-justify flex flex-col  max-h-[55vh] overflow-auto gap-3">
                         <li>
                             Web ini merupakan fasilitator penjual - pembeli dan pemberi kerja - pencari kerja untuk dapat berkomunikasi dan transaksi.
                         </li>
@@ -42,7 +42,7 @@ export default function DisclaimerModal ({ isOpen, setIsOpenClose, handlesubmit 
                         onClick={() => {
                             handlesubmit()
                         }}
-                        className="w-1/5 px-4 py-1 bg-zinc-800 text-slate-200 font-montserrat text-xl hover:outline hover:outline-2 hover:bg-transparent hover:outline-zinc-800 hover:text-zinc-800 transition-colors"
+                        className=" px-4 py-1 bg-zinc-800 text-slate-200 font-montserrat text-xl hover:outline hover:outline-2 hover:bg-transparent hover:outline-zinc-800 hover:text-zinc-800 transition-colors"
                     >
                         Setuju
                     </button>
