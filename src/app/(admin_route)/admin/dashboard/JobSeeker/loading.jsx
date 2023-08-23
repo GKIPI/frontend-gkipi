266f8c4a5b2b1f69f401a1314ca1b74077e9d124
PetrusAriaa <petrusariacr25@gmail.com>
2023-08-23 07:32:36 +0700

    fix: setup for maintenance deployment

--- src/app/(admin_route)/admin/dashboard/JobSeeker/loading.jsx
+++
@@ -1,9 +0,0 @@
-export default function BlurredOnLoad() {
-  return (
-    <>
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-white z-[900] flex justify-center items-center">
-        <div className="spinner"></div>
-      </div>
-    </>
-  );
-}
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,21 @@
+import Link from 'next/link'
 import Hero from './components/hero'
 import Menu from './components/menu'
 import Navbar from './components/navbar_hero'
 
 export default function Home() {
   return (
-    <main className='h-[100vh] flex flex-col justify-end overflow-x-hidden'>
-      <Navbar/>
-      <Hero/>
-      <Menu/>
+    <main className='h-[100vh] flex flex-col gap-8 justify-center items-center overflow-x-hidden'>
+      {/* <Navbar/> */}
+      {/* <Hero/> */}
+      {/* <Menu/> */}
+      <div>
+        <h1 className='font-poppins font-semibold text-5xl'>Website under Maintenance</h1>
+      </div>
+      <Link href={"/login"}>
+        <button className="bg-zinc-800 px-5 py-2 text-slate-200 hover:outline hover:outline-2 hover:outline-zinc-800 hover:bg-transparent hover:text-zinc-800 transition-colors duration-200">
+          Go To Login</button>
+      </Link>
     </main>
   )
 }
