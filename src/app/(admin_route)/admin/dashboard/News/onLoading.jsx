d293724285a97e2674e3c9b51ea28decfd786a13
PetrusAriaa <petrusariacr25@gmail.com>
2023-09-05 22:10:25 +0700

    feat: Content Writing Page

---
+++ src/app/(admin_route)/admin/dashboard/News/onLoading.jsx
@@ -0,0 +1,36 @@
+export const OnLoading = () => {
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex gap-4">
+        <div className="aspect-video rounded-md h-36 bg-slate-100 animate-pulse"></div>
+        <div className="flex flex-col gap-4 justify-end w-full">
+          <h1 className="w-1/2 h-6 bg-slate-100 animate-pulse"></h1>
+          <div className="flex flex-col gap-2">
+            <h1 className="w-3/4 h-4 bg-slate-100 animate-pulse"></h1>
+            <h1 className="w-3/4 h-4 bg-slate-100 animate-pulse"></h1>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <div className="aspect-video rounded-md h-36 bg-slate-100 animate-pulse"></div>
+        <div className="flex flex-col gap-4 justify-end w-full">
+          <h1 className="w-1/2 h-6 bg-slate-100 animate-pulse"></h1>
+          <div className="flex flex-col gap-2">
+            <h1 className="w-3/4 h-4 bg-slate-100 animate-pulse"></h1>
+            <h1 className="w-3/4 h-4 bg-slate-100 animate-pulse"></h1>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <div className="aspect-video rounded-md h-36 bg-slate-100 animate-pulse"></div>
+        <div className="flex flex-col gap-4 justify-end w-full">
+          <h1 className="w-1/2 h-6 bg-slate-100 animate-pulse"></h1>
+          <div className="flex flex-col gap-2">
+            <h1 className="w-3/4 h-4 bg-slate-100 animate-pulse"></h1>
+            <h1 className="w-3/4 h-4 bg-slate-100 animate-pulse"></h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
