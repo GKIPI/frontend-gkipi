e170bf7e6dc6802167b32ec46e3e7ab92c4933ac
inamnurul <aam.inam7310@gmail.com>
2023-09-09 23:24:44 +0700

    feat: fetch data on activity details

--- src/app/activity/[id]/page.js
+++ src/app/activity/[id]/page.js
@@ -1,25 +1,45 @@
-
+"use client"
 import ImageBg from "../../../../public/Activity.png"
 import Image from "next/image"
+import { useEffect, useState } from "react"
+import BlurredOnLoad from "@/app/loading"
 
 import Link from "next/link"
 import { FiArrowLeft } from "react-icons/fi"
 
-export default function activityDetails() {
+export default function activityDetails({ params }) {
+    const [data, setData] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetch(`/api/admin/activity/${params.id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setData(data);
+                setIsLoading(false)
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
     return (
-        <section className="flex flex-col">
-            <div className="absolute h-[40px] w-[40px] rounded-full m-3 hover:bg-primary hover:text-white">
-                <Link href={"/activity"}>
-                    <FiArrowLeft className="h-8 md:h-10 w-max" /> 
-                </Link>
-            </div>
-                <h1 className="m-3 text-3xl font-bold mt-[50px] self-center">This Is the most outstanding Title</h1>
-            <div className="w-[90vw] self-center max-h-[50vh] items-center flex overflow-hidden">
-                <Image src={ImageBg} />
-            </div>
-            <div className="max-w-[90vw] my-5 self-center">
-                <p className="whitespace-pre-wrap text-justify">             hjdnaklsndlkasndklanskldnas daslknjdklasnkdlas dklasndklasnlkdnasl dklasndlkasndklanskld asd asklndklasndklas dklasndklasnkldnaskldnaklsd asndklasndklanslkdnasdasndlkasdas dasdklansdklasndkla salkndklasndklas daskldnklasndklas dasklndklasndklasn</p>
-            </div>
-        </section>
+        <>
+            {isLoading ? <BlurredOnLoad /> :
+                <section className="flex flex-col">
+                    <div className="absolute h-[40px] w-[40px] rounded-full m-3 hover:bg-primary hover:text-white">
+                        <Link href={"/activity"}>
+                            <FiArrowLeft className="h-8 md:h-10 w-max" />
+                        </Link>
+                    </div>
+                    <h1 className="m-3 text-3xl font-bold mt-[50px] self-center">{data?.title}</h1>
+                    <div className="w-[90vw] self-center max-h-[50vh] items-center flex overflow-hidden justify-center">
+                        <img src={data?.image} className="self-center"/>
+                    </div>
+                    <div className="max-w-[90vw] my-5 self-center">
+                        <p className="whitespace-pre-wrap text-justify">{data?.details}</p>
+                    </div>
+                </section>
+            }
+        </>
     )
 }
